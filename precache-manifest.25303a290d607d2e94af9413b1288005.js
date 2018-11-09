8ca4c4fec726ce6302147215965bdf1b55aa9701
Katarina Khan <only4katka@gmail.com>
2018-11-09 16:18:04 +0200

    Updates

---
+++ precache-manifest.25303a290d607d2e94af9413b1288005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d106d487b9622f0b73f61ff8987251e",
+    "url": "/Crypto-React-Mobile-View/static/media/loader.4d106d48.svg"
+  },
+  {
+    "revision": "f22dbe0bd51b20d65bd2",
+    "url": "/Crypto-React-Mobile-View/static/js/runtime~main.f22dbe0b.js"
+  },
+  {
+    "revision": "417c5ba2c936b4e78a8e",
+    "url": "/Crypto-React-Mobile-View/static/js/main.417c5ba2.chunk.js"
+  },
+  {
+    "revision": "7d6f5a3f75ca164b6bb2",
+    "url": "/Crypto-React-Mobile-View/static/js/1.7d6f5a3f.chunk.js"
+  },
+  {
+    "revision": "417c5ba2c936b4e78a8e",
+    "url": "/Crypto-React-Mobile-View/static/css/main.879c3f54.chunk.css"
+  },
+  {
+    "revision": "1fef92c47dd4c2d560a5ce43d540d290",
+    "url": "/Crypto-React-Mobile-View/index.html"
+  }
+];
